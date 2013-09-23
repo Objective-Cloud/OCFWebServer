34773776be48ebbffe4c8267a29355679f1788d0
Christian Kienle <me@christian-kienle.de>
2013-09-23 17:44:02 +0200

    adds a framework target

---
+++ OCFWebServer Framework/OCFWebServer/OCFWebServer/WebServer.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+#import <OCFWebServer/OCFWebServer.h>
+#import <OCFWebServer/OCFWebServerRequest.h>
+#import <OCFWebServer/OCFWebServerResponse.h>
+#import <OCFWebServer/OCFWebServerRequest_Types.h>
---
+++ OCFWebServer Framework/OCFWebServer/OCFWebServerTests/OCFWebServerTests.m
@@ -0,0 +1,34 @@
+//
+//  OCFWebServerTests.m
+//  OCFWebServerTests
+//
+//  Created by cmk on 9/23/13.
+//  Copyright (c) 2013 Objective-Cloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCFWebServerTests : XCTestCase
+
+@end
+
+@implementation OCFWebServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
